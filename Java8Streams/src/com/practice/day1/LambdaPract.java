28733a10f07e961c74278486dcdcf64d619ff0e9
Mukesh <shunmukh20@gmail.com>
2018-10-16 11:11:33 +0530

    Added Streams and consumer code
    #Day2

--- Java8Streams/src/com/practice/day1/LambdaPract.java
+++ Java8Streams/src/com/practice/day1/LambdaPract.java
@@ -11,10 +11,10 @@ public class LambdaPract {
 		
 	//Consumer<String> c = (s) -> {System.out.println(s);};
 	Consumer<String> c = System.out::println;	
-		
+	Consumer<String> c1 = l -> System.out.println(l.length());	
 	List<String> l = Arrays.asList("hi","hello");	
 	
-	l.forEach(c);
+	l.forEach(c.andThen(c1));
 	
 Predicate<Integer> p = (i) -> i > 18 ;
 
---
+++ Java8Streams/src/com/stream/day2/StreamPract.java
@@ -0,0 +1,23 @@
+package com.stream.day2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamPract {
+
+	public static void main(String[] args) {
+		
+		List<String> l = Arrays.asList("s","h");
+		
+		l.stream().filter(s -> s.equals("s")).forEach(System.out::println);
+		
+		l.stream().map(s -> s.length()).forEach(System.out::println);
+
+		List<List<String>> ls = Arrays.asList(l,l);
+		
+		ls.stream().filter(f -> f.size() < 0).flatMap(m -> m.stream()).forEach(System.out::println);;
+	
+	     
+	}
+
+}
