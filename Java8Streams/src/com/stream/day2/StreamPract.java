226e27a29a5614ac400b8b939ea039cb2244c51a
Mukesh <shunmukh20@gmail.com>
2018-10-16 12:05:35 +0530

    Collectors,reduce and optional
    #Day2

--- Java8Streams/src/com/stream/day2/StreamPract.java
+++ Java8Streams/src/com/stream/day2/StreamPract.java
@@ -2,11 +2,14 @@ package com.stream.day2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class StreamPract {
 
 	public static void main(String[] args) {
-
+        
+		//Filter,map,FlatMap
 		List<String> l = Arrays.asList("s", "h");
 
 		l.stream().filter(s -> s.equals("s")).forEach(System.out::println);
@@ -18,9 +21,36 @@ public class StreamPract {
 		ls.stream().filter(f -> f.size() < 0).flatMap(m -> m.stream()).forEach(System.out::println);
 		;
 		
+		//Reduce
 		
+		List<Integer> li = Arrays.asList(2,3,4,5);
 		
-
+		System.out.println("All MAtch :"+li.stream().allMatch(a -> a > 0)); //anyMAtch,NoneMAtch are other extra methods
+		
+		System.out.println("Sum of Integer"+li.stream().reduce(0,Integer::sum));
+		
+		Optional<Integer> opt = li.stream().reduce(Integer::max);
+		
+		if(opt.isPresent())
+		{
+			System.out.println(opt.get());
+		}
+		else {
+			System.out.println(opt.isPresent());
+			try {
+				System.out.println(opt.orElseThrow(Exception:: new));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("Count of integre"+li.stream().count());
+		//Collectors
+		System.out.println(li.stream().collect(Collectors.counting()).intValue());
+		
+		System.out.println(li.stream().collect(Collectors.toList()));
+		
 	}
 
 }
