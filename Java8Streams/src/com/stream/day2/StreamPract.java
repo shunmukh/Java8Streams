074d4c6a74915586f153f9bf6c44b14de0651ca9
Mukesh <shunmukh20@gmail.com>
2018-10-16 11:49:51 +0530

    Formatted Files

--- Java8Streams/src/com/stream/day2/StreamPract.java
+++ Java8Streams/src/com/stream/day2/StreamPract.java
@@ -6,18 +6,21 @@ import java.util.List;
 public class StreamPract {
 
 	public static void main(String[] args) {
-		
-		List<String> l = Arrays.asList("s","h");
-		
+
+		List<String> l = Arrays.asList("s", "h");
+
 		l.stream().filter(s -> s.equals("s")).forEach(System.out::println);
-		
+
 		l.stream().map(s -> s.length()).forEach(System.out::println);
 
-		List<List<String>> ls = Arrays.asList(l,l);
+		List<List<String>> ls = Arrays.asList(l, l);
+
+		ls.stream().filter(f -> f.size() < 0).flatMap(m -> m.stream()).forEach(System.out::println);
+		;
 		
-		ls.stream().filter(f -> f.size() < 0).flatMap(m -> m.stream()).forEach(System.out::println);;
-	
-	     
+		
+		
+
 	}
 
 }
